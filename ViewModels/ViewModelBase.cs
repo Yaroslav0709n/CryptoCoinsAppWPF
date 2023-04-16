fa93765e2f280754dbabf7688938259f457ac69a
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2023-04-16 14:38:35 +0300

    Add files via upload

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Assignment.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyNone)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyNone));
+        }
+    }
+}
