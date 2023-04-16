a675ccbfdda53ac6c4038a83773343906888e9e2
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2023-04-16 14:43:45 +0300

    Add files via upload

---
+++ Checkers/SpecialCoinsId.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Assignment.Checkers
+{
+    internal class SpecialCoinsId
+    {
+        public static Dictionary<string, string> replacements = new Dictionary<string, string>()
+        {
+            { "bytecoin", "bytecoin-bcn" },
+            { "golem", "golem-network-tokens" },
+            { "iost", "iostoken" },
+            { "metaverse etp", "metaverse" },
+            { "nebulas", "nebulas-token" },
+            { "polymath", "polymath-network" }
+        };
+    }
+}
