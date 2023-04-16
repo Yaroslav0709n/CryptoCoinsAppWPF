cabd93bf4b4739c29f956d7ca04c88b3a18bc421
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2023-04-16 14:39:36 +0300

    Add files via upload

---
+++ Models/SomeDataExchangeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Assignment.Model
+{
+    public class SomeDataExchangeModel
+    {
+        public List<ExchangeModel> Data { get; set; }
+    }
+}
