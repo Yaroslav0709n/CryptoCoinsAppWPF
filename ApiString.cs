e2c274663f0852740c15394d11c8d2fda3e3dc74
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2023-04-16 14:33:00 +0300

    Add files via upload

---
+++ ApiString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Assignment
+{
+    public class ApiString
+    {
+        public const string ApiBaseUrl = "https://api.coincap.io/v2";
+        public const string ApiKey = "6d0c6fca-c85b-48be-9d36-4f25aa58c0fe";
+    }
+}
